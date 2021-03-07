7cba39dd8f95577a593637c318e11b3e574eb867
Michelle Lam <mlam8428@gmail.com>
2021-03-07 18:45:21 -0500

    add changes

--- README.md
+++ README.md
@@ -2,51 +2,9 @@
 a site for a local metalworking business. built with react, shards react and ❤️
 
 ## commands
-* to start app locally: `npm start`
-* after making changes, run: `npm run deploy`
-```
-npm run deploy
-```
-
-## to do (non dev)
-- [x] Look into how existing construction / metalwork websites look like
-- [ ] Come up with a design
-- [ ] Figure out how SEO works
-- [ ] Think of the copy for the site
-- [ ] Compile all the projects papa has worked on
-  * This involves pictures and details about the project
-  * Details: how long project is, what the was, where the project was
-
-## to do (dev)
-- [x] Set up ReactJS
-- [x] Integrate with [ant ui](https://ant.design/docs/react/use-with-create-react-app)
-- [ ] Come up with project structure and components
-- [ ] Create pages
-  - [ ] Homepage (hero, details, testimonial, contact)
-  - [ ] Sample project page (image, dimensions, cost)
-  - [ ] Gallery view
-- [x] [Deploy website to github.io](https://github.com/gitname/react-gh-pages)
-  - [x] [Deploy site](https://levelup.gitconnected.com/deploying-a-create-react-app-with-routing-to-github-pages-f386b6ce84c2)
-- [ ] Buy and set up domain
-- [ ] Write tests
-
-## dev learning
-- [ ] Read up on [yarn vs npm](https://blog.risingstack.com/yarn-vs-npm-node-js-package-managers/)
-
-### future considerations
-- [ ] Set up backend to store info
-- [ ] Come up with database schema
-- [ ] Figure out how to deploy to Heroku
-- [ ] Set up weekly chron job to sync with data from a Google Sheets
-  * This google sheets can be a way for mom or dad to add information about new projects without us having to programmatically add it ourselves
-
-## instructions
+* To set up application, run `npm install`
 * To start app locally, run `npm run start`
 * To deploy changes, run `npm run deploy`
-
-## resources
-- [Construction website examples](https://contractorgorilla.com/top-construction-websites-for-2019/)
-- [Alloy Mix Inc content](https://jwiz.com/jewish/alloy-mix-inc-76712.html)
 
 ## brand colors
 `#36413E`: Outer Space Crayola
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import construction from '../img/construction.jpg';
 import { Link } from "react-router-dom"
 
+import ContructionPerson from '../img/construction-person.jpg';
 import Gate_1_Picture_1 from '../img/projects/gates/Gate_1/IMG_20200215_125014.jpg';
 import Kitchen_2_Picture_1 from '../img/projects/kitchen/Fume Hood/IMG_20190819_163717.jpg';
 import Landscaping_1_Picture_1 from '../img/projects/landscaping/IMG_20200620_143407.jpg';
+
 
 import '../styles/Home.css';
 
@@ -19,27 +21,36 @@ const Home = () => {
         </h4>
         </h1>
       </div>
-      <div className="container lead">
+      <div className="container lead" style={{paddingBottom: 0}}>
         <div className="row alloy-row">
           <div className="col-md-6 alloy-content">
-            <h1>
+            <h1 className="title">
               Alloy
             </h1>
             <h2>
               /ˈaˌloi/
-            </h2>
+            </h2><br/>
             <p>
-              Brass, bronze, and stainless steel. What do those have in common? They’re alloys, and they can be found everywhere, from your cars to homes to wrist watches. Just as how alloys are used for everything, Alloy Mix Inc. is the only solution to your home improvement needs. 
+              Brass, bronze, and stainless steel. What do those have in common? They’re alloys, and they can be found everywhere, from your cars to homes to wrist watches. Just as how alloys are used for everything, Alloy Mix Inc. is <b className="highlight">the only solution</b> to your home improvement needs. 
             </p>
             <p>
               If you can dream up a better vision for your home, we can create it. We have completed various internal and external home remodeling and home renovation projects. We handle everything from bathroom and kitchen renovations to creating room dividers. We also restore flooring, railings, ceilings, fences, gates, and more. 
             </p>
-            <p>
-              And a bit more about us: we are a general contractor company that has served the New York City area for more than a decade. That’s over 10 years of satisfied customer service. So you know you can count on us to help you maintain, repair, and restore your home. Trust in Alloy Mix Inc.
-            </p>
+            <img src={ContructionPerson} className="side-image alloy-card-image" alt="bottom image"/>
+            
           </div>
           <div className="col-md-6">
-            <img className="side-image" src={construction} alt="side image"/>
+            <img className="side-image-right" src={construction} alt="side image"/>
+            <p className="alloy-content">
+              And a bit more about us: we are a general contractor company that has served the New York City area for more than a decade.
+            </p>
+            <p className="alloy-content">
+              That’s <b className="highlight">over 10 years</b> of satisfied customer service! So you know you can count on us to help you maintain, repair, and restore your home.
+              
+            </p>
+            <p className="emphasis">
+              Trust in Alloy Mix Inc.
+            </p>
           </div>
         </div>
       </div>
